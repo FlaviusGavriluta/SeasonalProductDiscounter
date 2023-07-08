d58316a17ffe726ea6359bfd2ce346cf3ce9d1da
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-08 13:22:33 +0300

    implement discount provider interface

---
+++ src/main/java/com/codecool/service/discounts/DiscountProviderImpl.java
@@ -0,0 +1,24 @@
+package com.codecool.service.discounts;
+
+import com.codecool.model.discounts.*;
+import com.codecool.model.enums.Season;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscountProviderImpl implements DiscountProvider{
+    private final List<Discount> discounts;
+    public DiscountProviderImpl() {
+        discounts = new ArrayList<>();
+        discounts.add(new MonthlyDiscount());
+        discounts.add(new ColorDiscount());
+        discounts.add(new SeasonalDiscount(Season.SUMMER));
+        discounts.add(new SeasonalDiscount(Season.WINTER));
+        discounts.add(new OutletDiscount(Season.SUMMER));
+        discounts.add(new OutletDiscount(Season.WINTER));
+    }
+    @Override
+    public List<Discount> getDiscounts() {
+        return discounts;
+    }
+}
