b401f8a2b86b678bfca9796d78c2fb4552a543ec
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-08 13:45:24 +0300

    refactor DiscountServiceImpl/ extern two methods

--- src/main/java/com/codecool/service/discounts/DiscountServiceImpl.java
+++ src/main/java/com/codecool/service/discounts/DiscountServiceImpl.java
@@ -5,44 +5,22 @@ import com.codecool.model.offers.Offer;
 import com.codecool.model.products.Product;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DiscountServiceImpl implements DiscountService {
     private final DiscountProvider discountProvider;
+    private final DiscountCalculator discountCalculator;
 
     public DiscountServiceImpl(DiscountProvider discountProvider) {
         this.discountProvider = discountProvider;
+        this.discountCalculator = new DiscountCalculator();
     }
 
     @Override
     public Offer getOffer(Product product, LocalDate date) {
-        List<Discount> applicableDiscounts = findApplicableDiscounts(product, date);
-        Double discountedPrice = calculateDiscountedPrice(product, applicableDiscounts);
-        return new Offer(product, date, applicableDiscounts, discountedPrice);
-    }
-
-    private List<Discount> findApplicableDiscounts(Product product, LocalDate date) {
         List<Discount> allDiscounts = discountProvider.getDiscounts();
-        List<Discount> applicableDiscounts = new ArrayList<>();
-
-        for (Discount discount : allDiscounts) {
-            if (discount.accepts(product, date)) {
-                applicableDiscounts.add(discount);
-            }
-        }
-
-        return applicableDiscounts;
-    }
-
-    private double calculateDiscountedPrice(Product product, List<Discount> discounts) {
-        double price = product.price();
-        double totalDiscount = 0;
-
-        for (Discount discount : discounts) {
-            totalDiscount += (price * discount.rate()) / 100;
-        }
-
-        return price - totalDiscount;
+        List<Discount> applicableDiscounts = discountCalculator.findApplicableDiscounts(product, date, allDiscounts);
+        Double discountedPrice = discountCalculator.calculateDiscountedPrice(product, applicableDiscounts);
+        return new Offer(product, date, applicableDiscounts, discountedPrice);
     }
 }
