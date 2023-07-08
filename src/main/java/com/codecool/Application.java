2f7c444ff7a1fee8b5b6ce3dca6160869f0e1014
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-08 14:19:29 +0300

    implement the last task

--- src/main/java/com/codecool/Application.java
+++ src/main/java/com/codecool/Application.java
@@ -1,7 +1,9 @@
 package com.codecool;
 
 import com.codecool.service.discounts.DiscountProvider;
+import com.codecool.service.discounts.DiscountProviderImpl;
 import com.codecool.service.discounts.DiscountService;
+import com.codecool.service.discounts.DiscountServiceImpl;
 import com.codecool.service.products.ProductProvider;
 import com.codecool.service.products.ProductProviderImpl;
 import com.codecool.ui.SeasonalProductDiscounterUi;
@@ -9,8 +11,8 @@ import com.codecool.ui.SeasonalProductDiscounterUi;
 public class Application {
     public static void main(String[] args) {
         ProductProvider productProvider = new ProductProviderImpl();
-        DiscountProvider discountProvider = null;
-        DiscountService discounterService = null;
+        DiscountProvider discountProvider = new DiscountProviderImpl();
+        DiscountService discounterService = new DiscountServiceImpl(discountProvider);
         SeasonalProductDiscounterUi ui = new SeasonalProductDiscounterUi(productProvider,
                 discountProvider, discounterService);
         ui.run();
